49bb0abbccecce327c00b5d681b434dbc8947205
Bryleigh Ballesteros <bab3@hawaii.edu>
2026-04-28 11:54:16 -1000

    Progress in-class 4/28

--- ProjectTwo_RestaurantApp/CalculatorViewController.swift
+++ ProjectTwo_RestaurantApp/CalculatorViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var enterBillButton: UIButton!
+    @IBOutlet weak var setTipLabel: UILabel!
+    
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var billAmountLabel: UILabel!
+    @IBOutlet weak var tipAmountDisplay: UILabel!
+    @IBOutlet weak var billAmountDisplay: UILabel!
+    
+    var billAmount = 0.00
+    var tipAmount = 0.00
+    var totalAmount = 0.00
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.text = "Tip Calculator"
+        subtitleLabel.text = "Guidelines for Tipping"
+        descriptionLabel.text = "When you go out to eat at a sit-down restaurant, you should tip 20%. And if you really want to be generous, go for the 25% mark. Remember: Most servers make around $2 per hour, so they're counting on those tips to make ends meet. You should consider the tip part of the overall experience of the meal."
+        
+        enterBillButton.setTitle("Enter the Bill Amount", for: .normal)
+        setTipLabel.text = "Set Tip %"
+        
+        tipAmountLabel.text = "Your Tip Amount:"
+        billAmountLabel.text = "Your Total Bill +\nTip Amount:"
+        tipAmountDisplay.text = ""
+        billAmountDisplay.text = ""
         // Do any additional setup after loading the view.
     }
     
